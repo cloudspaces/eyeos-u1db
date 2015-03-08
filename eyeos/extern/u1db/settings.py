c28018a7a8ded7428661cc134e67e71b693c37a0
Mustapha Koumach <mstafkmx@gmail.com>
2015-03-08 22:35:18 +0100

    Configuration settings.py changed, config Nec added.

--- eyeos/extern/u1db/settings.py
+++ eyeos/extern/u1db/settings.py
@@ -21,8 +21,23 @@ settings = {
         "urls": {
             "REQUEST_TOKEN_URL": "http://api.stacksync.com:8080/oauth/request_token",
             "ACCESS_TOKEN_URL": "http://api.stacksync.com:8080/oauth/access_token",
-            "CALLBACK_URL": "http://cloudspace1.eyeosbcn.com",
-            "RESOURCE_URL": "http://api.stacksync.com:8080/v1/"
+            "CALLBACK_URL": "http://sebasvm.eyeosbcn.com",
+            "RESOURCE_URL": "http://api.stacksync.com:8080/v1/",
+            "OAUTH_URL": "http://api.stacksync.com:8080/oauth/authorize?oauth_token="
+        },
+        "consumer": {
+            "key": "8224c4148302d09e287ba35fe96f214e0ac5b3c5c",
+            "secret" : "f88a48225a02542ec720dc18cba36023"
+        },
+        "version": "v2"
+    },
+    "Nec": {
+        "urls": {
+            "REQUEST_TOKEN_URL": "http://api.stacksync.com:8080/oauth/request_token",
+            "ACCESS_TOKEN_URL": "http://api.stacksync.com:8080/oauth/access_token",
+            "CALLBACK_URL": "http://sebasvm.eyeosbcn.com",
+            "RESOURCE_URL": "http://api.stacksync.com:8080/v1/",
+            "OAUTH_URL": "http://api.stacksync.com:8080/oauth/authorize?oauth_token="
         },
         "consumer": {
             "key": "8224c4148302d09e287ba35fe96f214e0ac5b3c5c",
